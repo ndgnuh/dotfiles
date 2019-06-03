272daf08719e661d12b436757eedbd5ebfdc2659
ndgnuh <ndgnuh99@gmail.com>
2019-06-04 00:48:02 +0700

    [wip]: new push script

--- music/adbpush.sh
+++ music/adbpush.sh
@@ -1,2 +1,9 @@
-ls | grep -v .sh | grep -v .js | grep -v list | xargs -I {} adb push {} /sdcard/Music
-adb shell am broadcast -a android.intent.action.MEDIA_MOUNTED -d file:///sdcard
+# ls | grep -v .sh | grep -v .js | grep -v list | xargs -I {} adb push {} /sdcard/Music
+# adb shell am broadcast -a android.intent.action.MEDIA_MOUNTED -d file:///sdcard
+cd music
+adb shell cd /sdcard/Music/; find | tee ../android_file > /dev/null
+find | tee ../host_file > /dev/null
+diff=$(diff ../android_file ../host_file)
+
+echo $diff | grep \> 
+cd ..
--- music/pull.sh
+++ music/pull.sh
@@ -7,7 +7,7 @@ realname=$(echo $1      | awk -F $delim '{print $2}' | sed 's/^ *//g'           
 artist=$(echo $1        | awk -F $delim '{print $3}' | sed 's/^ *//g'             | sed 's/ *$//g')
 album=$(echo $1         | awk -F $delim '{print $4}' | sed 's/^ *//g'             | sed 's/ *$//g')
 name=$(echo $realname   | tr '[:upper:]' '[:lower:]' | sed 's/ /-/g'              | sed "s/[:,'\(\)\.&\*]//g")
-musicdir=$(echo $artist | sed 's/ /-/g'              | tr '[:upper:]' '[:lower:]' | sed "s/[:,'\(\)\.&\*]//g"  )
+musicdir="music/$(echo $artist | sed 's/ /-/g'              | tr '[:upper:]' '[:lower:]' | sed "s/[:,'\(\)\.&\*]//g"  )"
 [[ -z $album ]] && musicdir="$musicdir/$(echo $album | sed 's/ /-/g' |  tr '[:upper:]' '[:lower:]' | sed "s/[:,'\.\(\)&\*]//g")"
 
 if [[ -f $musicdir/$name.$format ]]; then
