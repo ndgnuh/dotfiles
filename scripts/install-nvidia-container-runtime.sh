6b6454e1aafc20321e73236f661d5e5f593b9703
Hùng Nguyễn <ndgnuh@protonmail.com>
2023-07-04 16:49:29 +0700

    change key adding commands

--- scripts/install-nvidia-container-runtime.sh
+++ scripts/install-nvidia-container-runtime.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
-curl -s -L https://nvidia.github.io/nvidia-container-runtime/gpgkey | \
-  sudo apt-key add -
+curl -s -L https://nvidia.github.io/nvidia-container-runtime/gpgkey > /tmp/nvidia-container-runtime
+gnupg --dearmor /tmp/nvidia-container-runtime
+if [ $? -ne 0 ]; then
+  "Install CURL and GnuPG first"
+fi
+sudo mv /tmp/nvidia-container-runtime.gpg /etc/apt/trusted.gpg.d/
 distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+distribution=debian11
 curl -s -L https://nvidia.github.io/nvidia-container-runtime/$distribution/nvidia-container-runtime.list | \
   sudo tee /etc/apt/sources.list.d/nvidia-container-runtime.list
 sudo apt-get update
