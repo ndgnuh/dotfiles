d18fca23b3bb9a3e99d4d59eb8696a33704ebc1b
Hùng Nguyễn <ndgnuh@protonmail.com>
2023-07-04 15:51:28 +0700

    update ibus bamboo install script

--- scripts/install-ibus-bamboo.sh
+++ scripts/install-ibus-bamboo.sh
@@ -5,5 +5,9 @@
 # sudo apt update
 # sudo apt install ibus-bamboo
 
-wget https://download.opensuse.org/repositories/home:/lamlng/Debian_11/amd64/ibus-bamboo_0.8.1-0_amd64.deb -O ibus-bamboo.deb
-sudo nala install ./ibus-bamboo.deb
+# wget https://download.opensuse.org/repositories/home:/lamlng/Debian_11/amd64/ibus-bamboo_0.8.1-0_amd64.deb -O ibus-bamboo.deb
+# sudo nala install ./ibus-bamboo.deb
+echo 'deb http://download.opensuse.org/repositories/home:/lamlng/Debian_12/ /' | sudo tee /etc/apt/sources.list.d/home:lamlng.list
+curl -fsSL https://download.opensuse.org/repositories/home:lamlng/Debian_12/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/home_lamlng.gpg > /dev/null
+sudo apt update
+sudo apt install ibus-bamboo
