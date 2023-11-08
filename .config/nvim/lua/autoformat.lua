303a1b308ed07a0e22796cf587e6f5ed6ae588a4
Hùng Nguyễn <ndgnuh@protonmail.com>
2023-11-09 02:28:48 +0700

    update auto format

--- .config/nvim/lua/autoformat.lua
+++ .config/nvim/lua/autoformat.lua
@@ -7,47 +7,56 @@ local lsp_formatter = {}
 -- 2: formatter types
 -- 3: formatter command
 local formatters = {
-    -- python = "black -q - < %f",
-    python = "black %f",
-    lua = lsp_formatter,
+	python = { cmd = "black %f", is_inplace = true },
+	tex = { cmd = "latexindent %f", is_inplace = false },
+	lua = lsp_formatter,
 }
 
 local state = {
-    formatters = {},
-    is_on = true
+	formatters = {},
+	is_on = true,
 }
 
 local function toggle(hint)
-    if hint == nil then
-        state.is_on = not state.is_on
-    else
-        state.is_on = hint
-    end
+	if hint == nil then
+		state.is_on = not state.is_on
+	else
+		state.is_on = hint
+	end
 end
 
 local format_current_buffer = function()
-    local filetype = vim.bo.filetype
-    local format_type = formatters[filetype]
+	local filetype = vim.bo.filetype
+	local format_opt = formatters[filetype]
 
-    -- no formatter
-    if format_type == nil then
-        return
-    end
+	-- no formatter
+	if format_opt == nil then
+		return
+	end
 
-    -- lsp formatter
-    if format_type == lsp_formatter then
-        vim.lsp.buf.format()
-        vim.api.nvim_command("echo 'Formatted with LSP Formatter'")
-        return
-    end
+	-- lsp formatter
+	if format_opt == lsp_formatter then
+		vim.lsp.buf.format()
+		vim.api.nvim_command("echo 'Formatted with LSP Formatter'")
+		return
+	end
 
-    -- the command based formatter
-    local current_path = vim.fn.expand("%")
-    local cmd = format_type:gsub("%%f", current_path)
+	-- the command based formatter
+	local current_path = vim.fn.expand("%")
+	local cmd = format_opt.cmd:gsub("%%f", current_path)
 
-    -- formatted contents
-    local lines = vim.fn.system(cmd)
-    vim.api.nvim_command("e!")
+	-- formatted contents
+	local stdout = vim.fn.system(cmd)
+	if not format_opt.is_inplace then
+		local file = io.open(current_path)
+		if file ~= nil then
+			file:write(stdout)
+			file:close()
+		else
+			print("Failed to format file")
+		end
+	end
+	vim.api.nvim_command("e!")
 end
 
 _G.custom = {}
@@ -56,6 +65,6 @@ vim.api.nvim_create_user_command("MyAutoFormat", "lua _G.custom.format()", {})
 vim.api.nvim_create_autocmd("BufWrite", { buffer = 0, callback = format_current_buffer })
 
 return {
-    format = format_current_buffer,
-    lsp_formatter = lsp_formatter,
+	format = format_current_buffer,
+	lsp_formatter = lsp_formatter,
 }
